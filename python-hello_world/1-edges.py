f2b6cad67358bb370f42936ffc7e7f7976855c08
Tkilanko <tkilanko2@gmail.com>
2023-07-21 22:28:44 +0300

    1. Copy - Cut - Paste

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+#print("First 3 letters: {}".format(word_first_3))
+#print("Last 2 letters: {}".format(word_last_2))
+#print("Middle word: {}".format(middle_word))
+print(word[:3])
+print(word[-2:])
+print(word[1:8])
