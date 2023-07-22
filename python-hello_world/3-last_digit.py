714458c73a0cd8f119481ff323adf097f9e60dad
Tkilanko <tkilanko2@gmail.com>
2023-07-22 22:23:01 +0300

    reverted to previous version

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,11 +6,9 @@ number = random.randint(-10000, 10000)
 # Calculate the last digit of the number with its sign intact
 
 # Check the value of the last digit and print the result
-print('{}'.format(number))
-last_digit = str(number)[-1]
-if last_digit > '5':
-    print("Last digit of {} is {} and is greater than 5".format(number,last_digit ))
-elif last_digit == '0':
-    print("Last digit of {} is {} and is 0".format(number,str(number)[-1] ))
+if str(number)[-1] > '5':
+    print("Last digit of {} is {} and is greater than 5".format(number, str(number)[-1]))
+elif str(number)[-1] == '0':
+    print("Last digit of {} is {} and is 0".format(number, str(number)[-1]))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit ))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, str(number)[-1]))
