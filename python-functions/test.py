3dd84c05d92ecb320a326a52ce7fde04439fecca
Tkilanko <tkilanko2@gmail.com>
2023-07-25 20:12:11 +0300

    0. a + b

---
+++ python-functions/test.py
@@ -0,0 +1,20 @@
+'''while True:
+    try:
+        number  = int(input('Please enter a number: '))
+        break
+    except:
+        print('Please enter a valid number')
+        
+print(number)'''
+
+'''secret_number = 7
+while True:
+  #  try:
+    guess  = int(input('Please enter a number between 0 and 10: '))
+    if guess == secret_number:
+        print('Congrats, you got it') 
+        break       
+#except: '''
+ #   print('Wrong guess number')
+        
+#user_input = input('Enter a combination of words here: ')
