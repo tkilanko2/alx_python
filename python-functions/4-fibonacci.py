de6f4a769ccc4551daed03c20c941342013ac00f
Tkilanko <tkilanko2@gmail.com>
2023-07-26 22:40:31 +0300

    4. Fibonacci Sequence Function

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,1 +1,4 @@
-def fibonacci_sequence(n): fib_sequence = [0, 1]; return fib_sequence if n < 2 else fib_sequence + [fib_sequence.append(fib_sequence[-1] + fib_sequence[-2]) or fib_sequence[-1] + fib_sequence[-2] for _ in range(n - 2)]
+def fibonacci_sequence(n):
+    fib_sequence = [0, 1]
+    [fib_sequence.append(fib_sequence[-1] + fib_sequence[-2]) for _ in range(n - 2)]
+    return fib_sequence[:n]
