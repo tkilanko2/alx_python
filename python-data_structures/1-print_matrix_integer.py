d8808e0810e7802e5f5154e93ea7452d8dfb5aef
Tkilanko <tkilanko2@gmail.com>
2023-08-04 13:26:44 +0300

    Correction of "1. Lists of lists = Matrix"

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,8 +2,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+        for index, num in enumerate(row):
+            print("{:d}".format(num), end="")
+            if index < len(row) - 1:
+                print(" ", end="")
         print()
+
         
 
