c4d91b6f4b1dacfb298e78ae71dc4e1b9c60c859
Tkilanko <tkilanko2@gmail.com>
2023-08-05 21:55:30 +0300

    0. Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,15 @@
-def square_matrix_simple(matrix=[]):
+'''def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
     for idx, row in enumerate(new_matrix):
         for idx2, col in enumerate(new_matrix):
             new_matrix[idx][idx2] = row[idx2] ** 2
-    return new_matrix
+    return new_matrix '''
+    
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for row in matrix:
+        squared_row = [num ** 2 for num in row]
+        squared_matrix.append(squared_row)
+    return squared_matrix
+
+
